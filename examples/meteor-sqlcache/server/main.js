a3f9a0d307cf455afbd5af533eaa3f1061ba218b
Brian Rumburg <brian@rumburg.org>
2016-06-16 16:26:21 -0500

    Reshaped onMessage callback to use promises
    No longer need the explicit reject callback on subscribe API.  We now hop into the Meteor bound callback passing our own fulfil and reject callbacks and can return our new promise up the chain.

--- examples/meteor-sqlcache/server/main.js
+++ examples/meteor-sqlcache/server/main.js
@@ -1,21 +1,38 @@
 import { Meteor } from 'meteor/meteor';
 import mt from 'masstransit';
 import caches from './caches';
+import Promise from 'promise';
 
 Meteor.startup(() => {
   console.log('startup');
   
   var bus = mt.create();
 
-  var onMessage = Meteor.bindEnvironment((message, envelope, queueName, reject) => {
-    caches[message.table].refresh().catch((e) => {
-      reject(e);
-    });
+  var boundMessageHandler = Meteor.bindEnvironment(function(message, fulfill, reject) {
 
     if(message.table === 'Order') {
-      caches.RecentOrder.refresh().catch((e) => {
+
+      Promise.all([
+        caches.Order.refresh(),
+        caches.RecentOrder.refresh()
+      ])
+      .then(() => {
+        fulfill();
+      })
+      .catch((e) => {
         reject(e);
       });
+
+    } else {
+
+      caches[message.table].refresh()
+      .then(() => {
+        fulfill();
+      })
+      .catch((e) => {
+        reject(e);
+      });
+
     }
   });
 
@@ -23,7 +40,11 @@ Meteor.startup(() => {
     bus.subscribe({
       queueName: 'meteor-sqlcache',
       messageType: 'SqlNotifier.Messages:TableChanged'
-    }, onMessage);
+    }, (message) => {
+      return new Promise(function(fulfill, reject) {
+        boundMessageHandler(message, fulfill, reject);
+      });
+    });
   });
 
   bus.init({
